07a52b8fc361c31f47efd363dfa5c0565a0a14e6
Vincent Cartsvane <vincent.cartsvane@gmail.com>
2018-05-09 12:42:11 +0100

    Added URLs and a very basic view

--- cogaware/urls.py
+++ cogaware/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    # This means that anything that starts with e.g. 'distortions/blah/etc'
+    # will be passed onto the urls.py file in the distortions directory
+    # and processed from there as '/blah/etc'
+    path('distortions', include('distortions.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ distortions/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+# This is a relative import - "from . import views" means
+# "import views from the same directory as this file"
+# An absolute import would be "from cogaware.distortions import views"
+from . import views
+
+
+urlpatterns = [
+    # When you have an empty path, i.e. '/',
+    # call the index function from views.py
+    path('', views.index, name='index')
+]
