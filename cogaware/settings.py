02f4a04e148ac360d4eebdfe21631da4ec0521f9
Vincent Cartsvane <vincent.cartsvane@gmail.com>
2018-08-01 17:20:59 +0100

    Added a template

--- cogaware/settings.py
+++ cogaware/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'cogaware.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['../distortions/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
