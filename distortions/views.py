c84ef0fd694900df52a81d01bedd701232d5f368
Vincent Cartsvane <vincent.cartsvane@gmail.com>
2018-08-02 18:39:13 +0100

    Added list of traps in database to index page for traps

--- distortions/views.py
+++ distortions/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import TrapType
+
 
 def index(request):
-    context = {}
     template = loader.get_template("index.html")
+    context = {'traps': TrapType.objects.all()}
     return HttpResponse(template.render(context, request))
